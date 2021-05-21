1bb24b48ad858fec32576f6196a700f0930a658a
ibrahim Keskin <brhmkskn04@gmail.com>
2021-05-21 15:47:47 +0300

    photo controller save and delete functions

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,53 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken )
+        {
+            if(photo!=null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken); // cancelationToken foto/image vs yüklenme aşamasında browserın kapatılması vb işlemlerde yükleme işleminin durdurulmasını sağlar. Best Practise
+
+                // http://www.photostock.com/api/photos/aaaaa.jpg
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is Empty", 400));
+        }
+
+
+        public  IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if(!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
